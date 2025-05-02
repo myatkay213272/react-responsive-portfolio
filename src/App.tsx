5b88373923504fc17252f5d42825b8146b6e6860
mkzt <myatthant68@gmail.com>
2025-05-02 19:20:22 +0630

    Adding Chakra-ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import Register from './Register'
+// import Register from './Register'
 import Login from './Login'
+import { Box, Text, useColorMode ,Button} from '@chakra-ui/react'
 
 const App = () => {
+
+  const {colorMode,toggleColorMode} = useColorMode()
+
+
   return (
     <div>
-      <Register/>
+       <Box p={6} textAlign="center">
+      <Text mb={4}>Current mode: {colorMode}</Text>
+      <Button onClick={toggleColorMode}>
+        Switch to {colorMode === "light" ? "Dark" : "Light"} Mode
+      </Button>
+    </Box>
+
+   
       <Login/>
+      {/* <Register/> */}
     </div>
   )
 }
