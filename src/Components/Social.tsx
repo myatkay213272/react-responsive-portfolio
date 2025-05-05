edd264b36d9c0ca13aa308d05a124bf70ec0c1fc
mkzt <myatthant68@gmail.com>
2025-05-05 20:21:44 +0630

    Adding Icons

--- src/Components/Social.tsx
+++ src/Components/Social.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { HStack } from '@chakra-ui/react'
+import { faGoogle,faSpotify,faFacebook,faShopify } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const Social = () => {
   return (
-    <div>Social</div>
+    
+
+    <HStack spacing="18">
+
+    <FontAwesomeIcon icon={faFacebook} boxSize="50"/>
+    <FontAwesomeIcon icon={faGoogle} boxSize="50"/>
+    <FontAwesomeIcon icon={faSpotify} boxSize="50"/>
+    <FontAwesomeIcon icon={faShopify} boxSize="50"/>
+
+
+    </HStack>
+
   )
 }
 
