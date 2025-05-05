66447cc7837262d286ce9c8cef48ed848388f2e7
mkzt <myatthant68@gmail.com>
2025-05-05 19:43:35 +0630

    Adding header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import { Flex, Heading, IconButton, VStack } from "@chakra-ui/react"
-import { faSun,faMoon } from "@fortawesome/free-solid-svg-icons"
+import { Box, Flex, Heading, IconButton, useColorMode, VStack } from "@chakra-ui/react"
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram,faGithub ,faLinkedin }from "@fortawesome/free-brands-svg-icons";
-const App = () => {
+import { faInstagram, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Header from "./Components/Header";
+import Profile from "./Components/Profile";
+import Social from "./Components/Social";
 
+const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
 
   return (
-   
-  <VStack p={5}>
-    <Flex w="100%" justifyContent="space-between">
-        <Heading ml="8" size="md" fontWeight="semibold" color="cyan">
-            IntheTop
-        </Heading>
 
-        <FontAwesomeIcon icon={faInstagram}/>
+    <VStack p={5}>
+      <Flex w="100%" justifyContent="space-between" alignItems="center">
+        <Heading ml="8" size="md" fontWeight="semibold" color="cyan.300">ByteProfile</Heading>
 
+        <Box mr="8" display="flex" gap={4}>
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faGithub} />
+          <FontAwesomeIcon icon={faLinkedin} />
+          <IconButton
+            icon={<FontAwesomeIcon icon={isDark ? faSun : faMoon} />}
+            isRound={true}
+            size="sm"
+            onClick={toggleColorMode}
+            aria-label="Toggle dark mode"
+          />
+      </Box>
+      </Flex>
 
-    </Flex>
+      <Header/>
+      <Social/>
+      <Profile/>
     </VStack>
-  )
+
+  );
 }
 
-export default App
+export default App;
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import {
+  Stack, useColorMode, useMediaQuery,
+  Circle, Text, Flex, Box, Button, Image
+} from '@chakra-ui/react'
+import logo from "../assets/photo1.jpg"
+
 
 const Header = () => {
+
+ const {colorMode} = useColorMode()
+ const isDark = colorMode === "dark"
+ const [isNotSmallScreen] = useMediaQuery("(min-width:600px)");
+
   return (
-    <div>Header</div>
+    
+    <Stack>      
+    
+    <Circle
+        position="absolute"
+        bg="blue.100"
+        opacity="0.1"
+        w="300px"
+        h="300px"
+        alignSelf="flex-end"
+      />
+
+
+    <Flex 
+      direction={isNotSmallScreen ? "row" : "column"}
+      spacing="200px" 
+      p={isNotSmallScreen ? "32" : "0"}
+      alignSelf="flex-start"
+      align="center"
+      justify="center"
+      w="100%"
+    >
+
+      <Box mt={isNotSmallScreen ? "0" : 16} align="flex-start">
+        <Text fontSize="3xl" fontWeight="semibold">Hi, I am</Text>
+        <Text fontSize="5xl" fontWeight="bold" 
+          bgGradient="linear(to-r,cyan.400,blue.500,purple.600)" bgClip="text">
+          NovaCore
+        </Text>
+        <Text color={isDark ? "gray.200" : "gray.500"}>
+          Would you like suggestions based on a specific theme (e.g. dark mode, AI, minimal, etc.)?
+        </Text>
+        <Button mt={8} colorScheme='blue' onClick={() => window.open("https://pawan.live")}>
+          Click Me
+        </Button>
+      </Box>
+
+      <Image 
+        alignSelf="center" 
+        mt={isNotSmallScreen ? "0" : "12"}
+        mb={isNotSmallScreen ? "0" : "12"} 
+        borderRadius="full"
+        backgroundColor="transparent" 
+        boxShadow="lg" 
+        boxSize="300px"
+        src={logo}
+      />
+    </Flex>
+
+      
+    </Stack>
+
   )
 }
 
